028388de1f1f5cacbb54d8fcb49ffe24f6ce723e
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-09-29 10:03:05 +0200

    Comentarios: Entrada y salida de datos y tipos de datos en python.

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,1 +1,21 @@
-print("Hola Mundo en python")
+#Tipos de datos y variables
+
+#Numéricos
+#enteros -- int
+#real -- float
+#nombre -- str
+
+nombre="Julián"
+edad=17
+mayorOmenorEdad=False
+print (type (mayorOmenorEdad))
+
+#Entrada y salida de datos.
+#Salida con print
+
+print ("Buenos dias",nombre, "y tu edad es",edad)
+
+#Entrada de datos -- input()
+nombre = input ("Dime tu nombre: \n")
+edad= input("Dime tu edad: \n")
+print ("Buenos dias",nombre, "y tu edad es",edad)
