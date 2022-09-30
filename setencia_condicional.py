24046a918fe913ab85f46a4e25ff5ba31d17b3cc
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-09-30 11:12:16 +0200

    Nuevo ejemplo

---
+++ setencia_condicional.py
@@ -0,0 +1,11 @@
+#Sentencias condicionales 
+
+# Un programa que lea tu edad por teclado y te imprima por pantalla si eres mayor de edad o menor
+edad=0
+edad = (int) (input("Indique su edad"))
+
+if(edad>=18):
+    print ("Eres mayor de edad")
+else:
+    print ("Eres menor de edad")    
+
