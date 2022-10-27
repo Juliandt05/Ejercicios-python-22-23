55bdc6d29f71ab43bc999af9c6c7420573eb85cd
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-10-27 10:09:03 +0200

    Canciones descargar lista

--- Descargarcancion.py
+++ Descargarcancion.py
@@ -1,8 +1,20 @@
 from pytube import YouTube
+from pytube import Playlist
 def descargarCancion(url:str):
     youtube= YouTube(url)
     print(youtube.author)
     print("Descargando", youtube.title)
     cancion= youtube.streams.get_audio_only()
     cancion.download
-descargarCancion()
+
+def descargarlista (url:str):
+    playlist= Playlist(url)
+    for cancion in  playlist.videos:
+        print("Descargando cancion: ",cancion.title)
+        cancion.streams.get_audio_only().download("Canciones")
+        print("******************\n")
+        
+
+url="https://www.youtube.com/watch?v=HKNz0DWQXZw&list=PLVwXkzK42QWrWyeunQ_hi4OM8iUrBWUlq"
+descargarlista(url)
+
